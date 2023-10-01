2cdbdbcfb583ec428f9884b30a6b23e9f04046a5
9ani <123793591+9ani@users.noreply.github.com>
2023-10-01 21:14:28 +0600

    Delete USDtoTNGExchange.py

--- USDtoTNGExchange.py
+++
@@ -1,5 +0,0 @@
-from CurrencyExchangeStrategy import CurrencyExchangeStrategy
-
-class USDtoTNGExchange(CurrencyExchangeStrategy):
-    def convert(self, amount):
-        return round(amount * 478,79)
